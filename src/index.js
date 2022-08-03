251c6617cfb3f347e75f804aa909b6e87096337d
Andrei Rivera <andreirive2003@gmail.com>
2022-08-02 18:50:42 -0600

    Conected the routes carpet to the index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,22 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 
+import usersRoutes from "./routes/users.routes.js";
+import commentsRoutes from "./routes/comments.routes.js";
+import likesRoutes from "./routes/likes.routes.js";
+import tweetsRoutes from "./routes/tweets.routes.js";
+import followsRoutes from "./routes/follows.routes.js";
+
 const app = express();
 app.use(morgan("dev"));
 
 app.use(cors());
 app.use(express.json());
+app.use(usersRoutes);
+app.use(commentsRoutes);
+app.use(likesRoutes);
+app.use(tweetsRoutes);
+app.use(followsRoutes);
 
 app.listen(process.env.PORT || 8000, () => {
   console.log(`🚀 App working on port 8000`);
